ad68e815a6222c8a922a1e1f715baebaaffd508d
MoolintiVaishnavi <moolintivaishnavi@gmail.com>
2024-07-03 15:32:36 +0530

    Update DeptBooks.js

--- email-validation/src/components/DeptBooks.js
+++ email-validation/src/components/DeptBooks.js
@@ -1,12 +1,18 @@
 import React, { useEffect,useState } from "react";
 import { Link,useParams } from "react-router-dom";
 import './main.css';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+import { useNavigate } from 'react-router-dom';
 
 function DeptBooks(){
     const { dept } = useParams();
     const [books,setBooks] = useState([]);
     const [loading,setLoading] = useState(true);
+    const navigate = useNavigate();
+
+    const navigateHome = () => {
+        navigate('/home');
+    };
 
     useEffect(() => {
         const fetchBooks = async() => {
@@ -28,11 +34,21 @@ function DeptBooks(){
 
     return(
         <>
+        <div className="navigation">
+            <button onClick={navigateHome} className="nav-btn">
+                <span>Home </span>
+                <span>&gt;&gt;</span>
+            </button>
+            <button  className="nav-btn">
+                <span> {dept}</span>
+            </button>
+
+        </div>
         <div className="NAVBAR">
             <Navbar />
         </div>
         <div className="department-books">
-            <h2>Books in {dept} Department</h2>
+            <h2>Home {dept} Department</h2>
             <ul className="bookList">
                 {books.map(book => (
                     <li key={book._id}>
