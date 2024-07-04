10f546a8ee0a5a1e743dae57b2014f38b621a066
MoolintiVaishnavi <moolintivaishnavi@gmail.com>
2024-07-04 11:58:35 +0530

    Update BookDetails.js

--- email-validation/src/components/BookDetails.js
+++ email-validation/src/components/BookDetails.js
@@ -2,12 +2,23 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
 import './main.css';
+import bookcover from "./bookcover.jpg";
+import { useNavigate } from 'react-router-dom';
 
 function BookDetails() {
     const { id } = useParams();
     const [book, setBook] = useState(null);
     const [loading, setLoading] = useState(true);
     const [isIssued, setIsIssued] = useState(false);
+    const navigate = useNavigate();
+
+    const navigateHome = () => {
+        navigate('/home');
+    };
+
+    const handleDeptClick = () => {
+        navigate(`/department/${book.department}`)
+      };
 
     useEffect(() => {
         const fetchBook = async () => {
@@ -83,25 +94,40 @@ function BookDetails() {
     if (!book) {
         return <p>No book details found.</p>;
     }
-
     return (
         <>
             <div className="NAVBAR">
                 <Navbar />
             </div>
+            <div className="navigation">
+                <button onClick={navigateHome} className="nav-btn">
+                    <span>Home </span>
+                    <span>&gt;&gt;</span>
+                </button>
+                <button  className="nav-btn" onClick={handleDeptClick}>
+                    <span> {book.department}</span>
+                    <span>&gt;&gt;</span>
+                </button>
+                <button  className="nav-btn">
+                    <span>book details</span>
+                </button>
+            </div>
             <div className="book-details">
                 <h1>{book.title}</h1>
                 <div className="info">
-                    <p><strong>Author:</strong> {book.author}</p>
-                    <p><strong>Department:</strong> {book.department}</p>
-                    <p><strong>Genre:</strong> {book.genre}</p>
-                    <p><strong>Description:</strong> {book.description}</p>
-                    <p><strong>Number of units left:</strong> {book.count}</p>
-                    {isIssued ? (
-                        <p><button onClick={handleReturn}>RETURN</button></p>
+                    <div className='book-img'><img src={bookcover} className='bookImg'></img></div>
+                    <div>
+                        <p><strong>Author:</strong> {book.author}</p>
+                        <p><strong>Department:</strong> {book.department}</p>
+                        <p><strong>Genre:</strong> {book.genre}</p>
+                        <p><strong>Description:</strong> {book.description}</p>
+                        <p><strong>Number of units left:</strong> {book.count}</p>
+                        {isIssued ? (
+                        <p><button onClick={handleReturn} className="book-btn">RETURN</button></p>
                     ) : (
-                        <p><button onClick={handleIssue}>ISSUE</button></p>
+                        <p><button onClick={handleIssue} className="book-btn">ISSUE</button></p>
                     )}
+                    </div>
                 </div>
             </div>
         </>
