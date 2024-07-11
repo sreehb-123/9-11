a88da4f67bc3356670825d4424994094edc3432e
MoolintiVaishnavi <moolintivaishnavi@gmail.com>
2024-07-11 12:37:35 +0530

    Update Deptbox.js

--- email-validation/src/components/Deptbox.js
+++ email-validation/src/components/Deptbox.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './main.css'
 import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Deptbox() {
   const navigate = useNavigate();
@@ -13,12 +14,12 @@ export default function Deptbox() {
     <div className='branch-container'>
         <p>Browse using deparments</p>
         <ul>
-            <li><button onClick={() => handleDeptClick('Computer Science')} className="dept-button">Computer Science</button></li>
-            <li><button onClick={() => handleDeptClick('Electrical Engineering')} className="dept-button">Electrical Engineering</button></li>
-            <li><button onClick={() => handleDeptClick('Mechanical Engineering')} className="dept-button">Mechanical Engineering</button></li>
-            <li><button onClick={() => handleDeptClick('Civil Engineering')} className="dept-button">Civil Engineering</button></li>
-            <li><button onClick={() => handleDeptClick('Chemical Engineering')} className="dept-button">Chemical Engineering</button></li>
-            <li><button onClick={() => handleDeptClick('Engineering Physics')} className="dept-button">Engineering Physics</button></li>
+            <li><button onClick={() => handleDeptClick('Computer Science')} className="dept-button" > <i class="fa-solid fa-computer"></i> Computer Science</button></li>
+            <li><button onClick={() => handleDeptClick('Electrical Engineering')} className="dept-button"><i class="fa-solid fa-plug"></i> Electrical Engineering</button></li>
+            <li><button onClick={() => handleDeptClick('Mechanical Engineering')} className="dept-button"><i class="fa-solid fa-gears"></i> Mechanical Engineering</button></li>
+            <li><button onClick={() => handleDeptClick('Civil Engineering')} className="dept-button"><i class="fa-solid fa-helmet-safety"></i> Civil Engineering</button></li>
+            <li><button onClick={() => handleDeptClick('Chemical Engineering')} className="dept-button"><i class="fa-solid fa-flask"></i> Chemical Engineering</button></li>
+            <li><button onClick={() => handleDeptClick('Engineering Physics')} className="dept-button"><i class="fa-solid fa-lightbulb"></i>Engineering Physics</button></li>
         </ul>
     </div>
   )
