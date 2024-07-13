73e5b4bd560bd23a7af098d0fffb13dffb9478d7
MoolintiVaishnavi <moolintivaishnavi@gmail.com>
2024-07-13 19:22:05 +0530

    Update Homepage.js

--- email-validation/src/pages/Homepage.js
+++ email-validation/src/pages/Homepage.js
@@ -2,7 +2,7 @@ import './Homepage.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Deptbox from '../components/Deptbox';
-//import BookDisplay from './components/BookDisplay';
+import BookDisplay from '../components/BookDisplay';
 
 function Home() {
   return (
@@ -10,7 +10,7 @@ function Home() {
         <Navbar />
         <div>
           <Deptbox />
-          {/*<BookDisplay />*/}
+          <BookDisplay />
         </div>
       </div>
   );
