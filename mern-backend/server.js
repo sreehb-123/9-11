9a8af69021d23e8855030f54e80c9047b5a7ef0b
sreehb-123 <152023996+sreehb-123@users.noreply.github.com>
2024-09-07 10:42:44 +0530

    revert back

--- mern-backend/server.js
+++ mern-backend/server.js
@@ -12,12 +12,10 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb://localhost:27017/libraryDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+});
 
 const bookSchema = new mongoose.Schema({
     title: String,
